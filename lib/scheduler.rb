d51ff7cfea76d993ed31e195c259a8133afdc499
Jason Yuen <jasony@nulogy.com>
2015-05-01 11:37:48 -0400

    adding the ability to load demands

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -1,7 +1,26 @@
 require 'ai4r'
 
+Demand = Struct.new(:id, :minutes, :due_date)
+class Data
+  class << self
+    attr_accessor :demands
+  end
+
+  def self.load_demand(filename)
+    Data.demands = []
+    CSV.foreach(filename) do |row|
+      Data.demands << Demand.new(row[0].to_i, row[1].to_i, Time.parse(row[2]))
+    end
+  end
+end
+
 class SchedulingChromosome
   attr_accessor :normalized_fitness
+  attr_accessor :sequence
+
+  def initialize(demands)
+    @sequence = demands
+  end
 
   def fitness
     -1
@@ -11,17 +30,24 @@ class SchedulingChromosome
   end
 
   def self.reproduce(a, b)
-    SchedulingChromosome.new
+    a
   end
 
   def self.seed
-    SchedulingChromosome.new
+    SchedulingChromosome.new(Data.demands)
   end
 end
+
+puts "Initializing" 
+Data.load_demand('demands.csv')
+puts "Demand"
+p Data.demands
 
 puts "Beginning genetic search, please wait... "
 initial_population_size = 800
 generations = 1000
+
+
 search = Ai4r::GeneticAlgorithm::GeneticSearch.new(initial_population_size, generations, SchedulingChromosome)
 result = search.run
 puts result.fitness
